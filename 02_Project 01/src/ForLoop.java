26d29f173b93acad00498773112ef68d3a8efba5
Rafał Sabak <rafal.sabak@gmailexample.com>
2025-03-16 07:07:22 +0100

    added 'For Loop'

---
+++ 02_Project 01/src/ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+        // Pętla od 1 do 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Aktualna wartość i: " + i);
+        }
+    }
+}
+
